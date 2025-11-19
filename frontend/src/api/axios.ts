89a4ce8b4843f08cb4a9d6e20b6cae9df597d999
garg-lakshay <lakshaygarg775@gmail.com>
2025-11-19 15:06:41 +0530

    renamed API_BASE_URL to REACT_APP_API_BASE_URL

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useAuthStore } from "../store/auth";
 
-const API_BASE_URL = process.env.API_BASE_URL || "";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "";
 // Create axios instance
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`, // your backend base URL
